de0929c6691a67a839a45aa43c4cff237c71a8bf
Yuliya Belkovich <63455033+YuliyaBelkovich@users.noreply.github.com>
2020-05-06 15:34:26 +0300

    Add files via upload

---
+++ Book/Book.java
@@ -0,0 +1,78 @@
+package by.epam.javaIntro.simpleClass.Book;
+
+import java.io.FileNotFoundException;
+
+public class Book {
+
+    /*Создать класс Book, спецификация которого приведена ниже.
+    Определить конструкторы, set- и get- методы и метод toString().
+    Создать второй класс, агрегирующий массив типа Book,
+    с подходящими конструкторами и методами. Задать критерии выбора данных и вывести эти данные на консоль.
+    Book: id, название, автор(ы), издательство, год издания, количество страниц, цена, тип переплета.
+    Найти и вывести:
+    a) список книг заданного автора;
+    b) список книг, выпущенных заданным издательством;
+    c) список книг, выпущенных после заданного года.
+     */
+
+    private int id;
+    private String name;
+    private String author;
+    private String publishing;
+    private int yearPublished;
+    private int numberOfPages;
+    private int price;
+    private String typeOfBinding;
+
+    public Book(int id, String name, String author, String publishing, int yearPublished, int numberOfPages, int price, String typeOfBinding) {
+        this.id = id;
+        this.name = name;
+        this.author = author;
+        this.publishing = publishing;
+        this.yearPublished = yearPublished;
+        this.numberOfPages = numberOfPages;
+        this.price = price;
+        this.typeOfBinding = typeOfBinding;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getPublishing() {
+        return publishing;
+    }
+
+    public int getYearPublished() {
+        return yearPublished;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", author='" + author + '\'' +
+                ", publishing='" + publishing + '\'' +
+                ", yearPublished=" + yearPublished +
+                ", numberOfPages=" + numberOfPages +
+                ", price=" + price +
+                ", typeOfBinding='" + typeOfBinding + '\'' +
+                '}';
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        Library bookShell = new Library();
+
+        bookShell.getBooksAfterYear(2000);
+        bookShell.getBooksByAuthor("Harper Lee");
+        bookShell.getBooksByPublishing("Arrow");
+    }
+
+}
+
---
+++ Book/Library.java
@@ -0,0 +1,59 @@
+package by.epam.javaIntro.simpleClass.Book;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Library {
+
+    private Book [] bookShell;
+
+    public Library() throws FileNotFoundException {
+        File bookShell = new File("C:\\Users\\Asus\\IdeaProjects\\module-4-simple-class\\src\\by\\epam\\javaIntro\\simpleClass\\Book\\BookShell.txt");
+        Scanner sc = new Scanner(bookShell);
+
+        int i = 0;
+        this.bookShell = new Book[6];
+
+        while(sc.hasNextLine()){
+            String [] tmp = sc.nextLine().split(",");
+            this.bookShell[i] = new Book(Integer.parseInt(tmp[0]),
+                    tmp[1],
+                    tmp[2],
+                    tmp[3],
+                    Integer.parseInt(tmp[4]),
+                    Integer.parseInt(tmp[5]),
+                    Integer.parseInt(tmp[6]),
+                    tmp[7]);
+            i++;
+        }
+    }
+
+    public void getBooksByAuthor(String author){
+        System.out.println("Books by "+author+ ":");
+        for(Book book : this.bookShell ){
+            if(book.getAuthor().equals(author)){
+                System.out.println(book.getName());
+            }
+        }
+    }
+
+    public void getBooksByPublishing(String publishing){
+        System.out.println("Books published by "+publishing+ " publishing:");
+        for(Book book : this.bookShell){
+            if(book.getPublishing().equals(publishing)){
+                System.out.println(book.getName());
+            }
+        }
+    }
+
+    public void getBooksAfterYear(int year) {
+        System.out.println("Books published after " + year + " year:");
+        for (Book book : this.bookShell) {
+            if (book.getYearPublished() > year) {
+                System.out.println(book.getName());
+            }
+        }
+    }
+}
+
